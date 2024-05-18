8c19e7ffac519aff90500dd933cc556caccfdf98
Shivam <srivashivam20@gmail.com>
2024-05-18 17:19:05 +0530

    Login/register form bulid

--- src/app/components/formdata/formdata/formdata.component.ts
+++ src/app/components/formdata/formdata/formdata.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,Input} from '@angular/core';
 import {  FormBuilder, FormControl, FormGroup, Validators,  AbstractControl } from '@angular/forms';
 import Validation from '../../../utils/validation';
 
@@ -9,7 +9,9 @@ import Validation from '../../../utils/validation';
   styleUrls: ['./formdata.component.css']
 })
 export class FormdataComponent implements OnInit {
-  form: FormGroup = new FormGroup({
+  @Input() componentName!: string;
+  register_bool:boolean=false;
+  register_form: FormGroup = new FormGroup({
     fullname: new FormControl(''),
     username: new FormControl(''),
     email: new FormControl(''),
@@ -17,10 +19,18 @@ export class FormdataComponent implements OnInit {
     confirmPassword: new FormControl(''),
     acceptTerms: new FormControl(false),
   });
+
+  login_form: FormGroup = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
   submitted = false;
   constructor(private formBuilder: FormBuilder) {} 
   ngOnInit(): void {
-    this.form = this.formBuilder.group(
+  console.log("Component Name : " ,this.componentName)
+  if(this.componentName=='register') this.register_bool=true;
+  else this.register_bool=false;
+    this.register_form = this.formBuilder.group(
       {
         fullname: ['', Validators.required],
         username: [
@@ -47,25 +57,53 @@ export class FormdataComponent implements OnInit {
         validators: [Validation.match('password', 'confirmPassword')],
       }
     );
+    ////////////////////////////////////////////////
+    this.login_form = this.formBuilder.group(
+      {
+        email: ['', [Validators.required, Validators.email]],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(40),
+          ],
+        ],
+      }
+    );
   }
 
   get f(): { [key: string]: AbstractControl } {
-    return this.form.controls;
+    return this.register_form.controls;
   }
 
-  onSubmit(): void {
+  onRegisterSubmit(): void {
     this.submitted = true;
 
-    if (this.form.invalid) {
+    if (this.register_form.invalid) {
       return;
     }
 
-    console.log(JSON.stringify(this.form.value, null, 2));
+    console.log(JSON.stringify(this.register_form.value, null, 2));
   }
 
-  onReset(): void {
+  onLoginSubmit():void{
+    this.submitted = true;
+
+    if (this.login_form.invalid) {
+      return;
+    }
+
+    console.log(JSON.stringify(this.login_form.value, null, 2));
+  }
+  
+  onRegisterReset(): void {
     this.submitted = false;
-    this.form.reset();
+    this.register_form.reset();
+  }
+  onLoginReset():void{
+    this.submitted = false;
+    this.login_form.reset();
   }
 }
 
